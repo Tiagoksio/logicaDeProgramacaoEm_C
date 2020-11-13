9dcff380fbae6223e6ef825f482790f212b8faf1
Tiago Oliveira <67693562+Tiagoksio@users.noreply.github.com>
2020-11-13 18:53:28 -0300

    sizeOff

---
+++ Comandos de entrada e saída/sizeOff.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+
+void main(){
+    int a, b, c, d, e, f, g;
+  
+
+    a=sizeof(int);
+    b=sizeof(long);
+    c=sizeof(short);
+    d=sizeof(float);
+    e=sizeof(char);
+    f=sizeof(long int);
+    g=sizeof(double);
+    
+    printf("%d\n", a);
+    printf("%d\n", b);
+    printf("%d\n", c);
+    printf("%d\n", d);
+    printf("%d\n", e);
+    printf("%d\n", f);
+    printf("%d\n", g);
+
+}
