e7287be81c9c596e793e2c6fe826dfd26b68bd0b
Tiago Oliveira <67693562+Tiagoksio@users.noreply.github.com>
2020-11-17 13:09:17 -0300

    atualização-valor-media

--- Vetores e Matrizes/notas-acima-igual-media.c
+++ Vetores e Matrizes/notas-acima-igual-media.c
@@ -10,10 +10,10 @@ void main(){
         scanf("%f", &notas[posicao]);
         soma=soma+notas[posicao];
     }
-    media = soma/20;
+    media = soma/5;
     printf("\nA média é: %.2f", media);
 
-    for (posicao = 0; posicao < 20; posicao++)
+    for (posicao = 0; posicao < 5; posicao++)
     {
         if (notas[posicao] >= media)
         {
