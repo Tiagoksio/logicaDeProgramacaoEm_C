0adb3b18e0f8ca8f9912f82fd071644f6e837947
Tiago Oliveira <67693562+Tiagoksio@users.noreply.github.com>
2020-11-18 00:34:24 -0300

    dias-da-seman

---
+++ Estruturas de decisão/dias-da-semana.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+
+void main (){
+    int opcao;
+
+    do
+    {
+        printf("Digite um número de 1 a 7 para selecionar um dia da semana\n");
+        scanf("%d", &opcao);
+        
+        switch (opcao)
+        {
+        case 1:
+            printf("Domingo\n");
+            break;
+    
+        case 2:
+            printf("Segunda-feira\n");
+            break;
+    
+        case 3:
+            printf("Terça-feira\n");
+            break;
+    
+        case 4:
+            printf("Quarta-feira\n");
+            break;
+
+        case 5:
+            printf("Quinta-feira\n");
+            break;
+
+        case 6:
+            printf("Sexta-feira\n");
+            break;
+
+        case 7:
+            printf("Sábado\n");
+            break;
+
+        default:
+            printf("ERRO! Opção inválida\n");
+            break;
+        }
+    } while (opcao>7 || opcao<0);
+       
+}
