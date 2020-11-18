fd8fadca9698d90aee4bbcd6797937a932e24a48
Tiago Oliveira <67693562+Tiagoksio@users.noreply.github.com>
2020-11-18 03:06:34 -0300

    operadores compostos de atribuição

---
+++ Operadores/atribuicao-composto.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+void main (){
+    
+    int num;
+    
+    num = 1; // atribui o valor 1 à variável 
+    printf("Atribuindo 1 a num = %d\n", num);
+    
+    num += 1; // num = num+1 
+    printf("Somando 1 a num = %d\n", num);
+    
+    num -= 1; // num = num-1 
+    printf("Subtraindo 1 de num = %d\n", num); 
+    
+    num *= 2; // num = num*2
+    printf("MUltiplicando num por 2 = %d\n", num); 
+    
+    num /= 2; // num = num/2 
+    printf("Dividindo num por 2 = %d\n", num); 
+       
+}
+   
+
