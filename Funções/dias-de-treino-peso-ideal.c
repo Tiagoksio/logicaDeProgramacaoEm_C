10d3977e78477c9bfa4953a631255a86eed331a8
Tiago Oliveira <67693562+Tiagoksio@users.noreply.github.com>
2020-11-20 01:04:17 -0300

    dias de treino refatorado

--- Funções/dias-de-treino-peso-ideal.c
+++ Funções/dias-de-treino-peso-ideal.c
@@ -1,29 +1,58 @@
 #include<stdio.h>
+#include <string.h>
 
-float ganhoDeMassa(int idade, float peso){
-    if (idade<25)
+float ganhoDeMassa(int idade, float peso);
+float perdaDePeso(int idade, float peso);
+
+int main (void){
+    
+    struct fichaUsuario
     {
-        peso = peso + (peso*0.05);
+        char nomeUsuario[50];
+        int idadeUsuario;
+        float pesoInicial, pesoDesejado, pesoFinal;
+    };
+
+    struct fichaUsuario aluno;
+    int diasDeTreino;  
+
+    diasDeTreino = 0;
+
+    printf("\n---------------- Cadastro de usuário ----------------\n\n\n");
+        
+    printf("Informe seu nome: ");
+    fgets(aluno.nomeUsuario,50,stdin);
+    printf("Informe sua idade: ");
+    scanf("%d", &aluno.idadeUsuario);
+    printf("Informe seu peso atual em 'kg': ");
+    scanf("%f", &aluno.pesoInicial);
+    printf("Informe seu o peso que deseja alcançar em 'kg': ");
+    scanf("%f", &aluno.pesoDesejado);
+    printf("\n----------------------------------------------------\n\n\n");
+    aluno.pesoFinal=aluno.pesoInicial;
+ 
+    if (aluno.pesoInicial>aluno.pesoDesejado)
+    {
+        
+        while (aluno.pesoFinal>aluno.pesoDesejado)
+        {
+            aluno.pesoFinal = perdaDePeso(aluno.idadeUsuario, aluno.pesoFinal);
+            diasDeTreino++;
+        }
+
     }
     else
     {
-        if (idade<30)
+        while (aluno.pesoFinal<aluno.pesoDesejado)
         {
-            peso = peso + (peso*0.03);
+            aluno.pesoFinal = ganhoDeMassa(aluno.idadeUsuario, aluno.pesoFinal);
+            diasDeTreino++;
         }
-        else
-        {
-            if (idade<50)
-            {
-                peso = peso + (peso*0.02);
-            }
-            peso = peso + (peso*0.01);
-        }
+        
+    }    
+    printf("\nSr(a).%s, em %d dias de treino você ficará com %.2fkg.\n", aluno.nomeUsuario, diasDeTreino, aluno.pesoFinal);
 
-    }
-    
-    return peso;
-};
+}
 
 float perdaDePeso(int idade, float peso){
     if (idade<25)
@@ -48,50 +77,31 @@ float perdaDePeso(int idade, float peso){
     }
     
     return peso;
-};
+}
 
-int main (void){
-    
-    char nomeUsuario[10];
-    int idadeUsuario, diasDeTreino;
-    float pesoUsuario, pesoDesejado;
-
-    diasDeTreino = 0;
-        
-    printf("Informe seu nome: ");
-    scanf("%s", &nomeUsuario[0]);
-
-    printf("Informe sua idade: ");
-    scanf("%d", &idadeUsuario);
-
-    printf("Informe seu peso atual em 'kg': ");
-    scanf("%f", &pesoUsuario);
-
-    printf("Informe seu o peso que deseja alcançar em 'kg': ");
-    scanf("%f", &pesoDesejado);
-
-   
- 
-    if (pesoUsuario>pesoDesejado)
+float ganhoDeMassa(int idade, float peso){
+    if (idade<25)
     {
-        while (pesoUsuario>pesoDesejado)
-        {
-            pesoUsuario = perdaDePeso(idadeUsuario, pesoUsuario);
-            diasDeTreino++;
-        }
-
+        peso = peso + (peso*0.05);
     }
     else
     {
-        while (pesoUsuario<pesoDesejado)
+        if (idade<30)
         {
-            pesoUsuario = ganhoDeMassa(idadeUsuario, pesoUsuario);
-            diasDeTreino++;
+            peso = peso + (peso*0.03);
         }
-        
-    }    
-    printf("\nSr(a).%s, em %d dias de treino você ficará com %0.2fkg\n", &nomeUsuario[0], diasDeTreino, pesoUsuario);
+        else
+        {
+            if (idade<50)
+            {
+                peso = peso + (peso*0.02);
+            }
+            peso = peso + (peso*0.01);
+        }
 
+    }
+    
+    return peso;
 }
 
 
