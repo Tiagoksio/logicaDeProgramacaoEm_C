ac0d9ffd3d537642856d5b6dd6b03e018351187f
Tiago Oliveira <67693562+Tiagoksio@users.noreply.github.com>
2020-11-26 00:46:08 -0300

    Conceitos de casting

---
+++ Comandos de entrada e saída/casting.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+void main(){
+    int x = 16;
+    int y = 3;
+    float resultado;
+
+    //Por conta das duas variáveis serem int, o resultado atribuído à variavel float, será a divisão por inteiro
+    resultado = x/y;
+    printf("%f\n", resultado);
+
+    /*Para que a divisão entre os inteiros seja atribuída calculando as casas decimais,
+    é necessária a operação de 'casting', que efetuará uma conversar para outro tipo */
+    resultado = (float)x/y;
+    printf("%f\n", resultado);
+
+}
