ab09e5877cd08917eda22503b0b0e5a09fac1436
Tiago Oliveira <67693562+Tiagoksio@users.noreply.github.com>
2021-02-07 05:50:23 -0300

    caracteres de controle

---
+++ Comandos de entrada e saída/caracteresDeControle.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(){
+    
+    printf("alarme sonoro\a");//\a - Alarme sonoro do sistema;
+    printf("Move o cursor uma posição à esquerda\b");//\b - Move o cursor uma posição à esquerda;
+    printf("Próxima linha\n");//\n - Próxima linha;
+    printf("tabulação\t");//\t - tabulação;
+    printf("início da linha\r");//\r - início da linha;
+    printf("Caractere nulo\0");//\0 - Caractere nulo, fim da string;
+    printf("\"Escreve entre aspas\"\0");//\" - entre aspas;
+    return 0;
+}
